0411c1a48919546eb2a943c07c9e2108a9cd91ce
dallo <48526000+dialloth@users.noreply.github.com>
2022-07-28 21:12:06 +0200

    Initialize moncompte

---
+++ pages/_app.js
@@ -0,0 +1,27 @@
+import "../styles/globals.css";
+import { appWithTranslation } from "next-i18next";
+import { SelectCartProvider } from "../contexts/selectCartContext";
+import { SessionProvider } from "next-auth/react";
+import { CurrentUserContextProvider } from "../contexts/currentUserContext";
+import { ToastProvider } from "react-toast-notifications";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ToastProvider>
+        <SessionProvider session={session}>
+          <CurrentUserContextProvider>
+            <SelectCartProvider>
+              <Component {...pageProps} />
+            </SelectCartProvider>
+          </CurrentUserContextProvider>
+        </SessionProvider>
+      </ToastProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default appWithTranslation(MyApp);
