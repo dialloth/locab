0543a257503aa2c98965dc1be4fec53c8530bbfc
dallo <48526000+dialloth@users.noreply.github.com>
2022-07-28 21:12:02 +0200

    Initialize authentification

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+import { CurrentUserContextProvider } from "../contexts/currentUserContext";
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <CurrentUserContextProvider>
+        {" "}
+        <Component {...pageProps} />{" "}
+      </CurrentUserContextProvider>
+    </SessionProvider>
+  );
+}
